a8330006f09d55342e63d432f1ff5becf6cd5dee
Martijn Oppedijk <martijn@meningreen.nl>
2016-01-12 19:11:51 +0100

    uncommited files

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -15,37 +15,41 @@ var carsRouter = extend(abstractRouter, {
 	getCarsData: function() {
 
 		var data = {
-				title: 'SSS | Cars', 
-				headerTitle: 'SSS',
-				menuItems: [
-					{title: 'Home', hash: '/'}, 
-					{title: 'Auto\'s', hash: 'cars', active: true},
-				],
-				cars: [
-					{
-					brand: "Toyota",
-					model: "Celica"
-					},
-					{
-					brand: "Ferrari",
-					model: "F40"
-					},
-					{
-					brand: "Tesla",
-					model: "X"
-					},
-					{
-					brand: "BMW",
-					model: "i118"
-					},
-					{
-					brand: "Audi",
-					model: "A4"
-					}
-				]
-			}
+			title: 'SSS | Cars', 
+			headerTitle: 'SSS',
+			menuItems: [
+				{title: 'Home', hash: '/'}, 
+				{title: 'Auto\'s', hash: 'cars', active: true},
+			],
+			cars: this.fetchCars()
+		};
+
+		console.log(data);
 
 		return data;
+	},
+	fetchCars: function() {
+
+		var resultSet = false;
+
+		this.MongoClient.connect('mongodb://localhost:27017/serverSideScripting', function(err, db) {
+			if (err) throw err;
+
+			db.collection('cars').find().toArray(function(err, result) {
+				if (err) throw err;
+
+				resultSet = result;
+
+				// return result;
+			}.bind(this));
+
+		}.bind(this));
+
+		console.log(resultSet);
+
+		// console.log(typeof this.resultSet);
+
+		return resultSet;
 	}
 });
 
